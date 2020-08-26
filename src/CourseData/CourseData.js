f937bea697f23d2d97a8b6fce48c678d3263d0b8
masum065 <masumbillah065@gmail.com>
2020-08-27 04:23:26 +0600

    Added Course Data

--- src/CourseData/CourseData.js
+++ src/CourseData/CourseData.js
@@ -32,15 +32,147 @@ var courses = [{
         "coverPhoto": "https://img-a.udemycdn.com/course/240x135/364426_2991_5.jpg"
     },
     {
-        "key": "JSG4764",
+        "key": "KCDTS764",
         "title": "Kids Coding - Beginner HTML",
-        "instructor": "Anthony Alicea",
+        "instructor": "John Bura",
         "regularPrice": "19.99",
         "price": "8.99",
-        "enrolled": 1547,
+        "enrolled": 6930,
         "reviewed": 347,
         "star": 4.5,
         "coverPhoto": "https://img-a.udemycdn.com/course/240x135/65492_33e5_5.jpg"
+    },
+    {
+        "key": "JAVKSHY564",
+        "title": "Java Pro Masterclass for Software Developers",
+        "instructor": "Goran Lochert",
+        "regularPrice": "259.99",
+        "price": "15.99",
+        "enrolled": 12500,
+        "reviewed": 1247,
+        "star": 4.9,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/533682_c10c_4.jpg"
+    },
+    {
+        "key": "UGD2D4645",
+        "title": "Complete C# Unity Game Developer 2D",
+        "instructor": "Rick Davidson",
+        "regularPrice": "155.99",
+        "price": "31.99",
+        "enrolled": 36785,
+        "reviewed": 7783,
+        "star": 4.5,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/258316_55e9_8.jpg"
+    },
+    {
+        "key": "PDHHD65DG",
+        "title": "Python for Data Science and Machine Learning",
+        "instructor": "Jose Portilla",
+        "regularPrice": "325.99",
+        "price": "40.99",
+        "enrolled": 16584,
+        "reviewed": 7754,
+        "star": 4.6,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/903744_8eb2.jpg"
+    },
+    {
+        "key": "CJCSKHF54",
+        "title": "The Complete JavaScript Course 2020",
+        "instructor": "Jonas Schmedtmann",
+        "regularPrice": "125.99",
+        "price": "11.99",
+        "enrolled": 65842,
+        "reviewed": 3322,
+        "star": 4.8,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/851712_fc61_5.jpg"
+    },
+    {
+        "key": "CNJSHF543",
+        "title": "The Complete Node.js Developer Course",
+        "instructor": "Rob Percival",
+        "regularPrice": "84.99",
+        "price": "16.99",
+        "enrolled": 198945,
+        "reviewed": 4985,
+        "star": 4.7,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/922484_52a1_8.jpg"
+    },
+    {
+        "key": "CDMLGH5431",
+        "title": "The Complete Android N Developer Course",
+        "instructor": "Marc Stock",
+        "regularPrice": "154.99",
+        "price": "12.99",
+        "enrolled": 14358,
+        "reviewed": 2685,
+        "star": 4.4,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/951618_0839_2.jpg"
+    },
+    {
+        "key": "ACGD3453",
+        "title": "Android Architecture Masterclass",
+        "instructor": "Vasiliy Zukanov",
+        "regularPrice": "99.99",
+        "price": "18.99",
+        "enrolled": 4829,
+        "reviewed": 685,
+        "star": 4.8,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/1803806_162c_3.jpg"
+    },
+    {
+        "key": "RNALKHF546",
+        "title": "React Native - The Practical Guide [2020 Edition]",
+        "instructor": "Maximilian Schwarzmüller",
+        "regularPrice": "158.99",
+        "price": "9.99",
+        "enrolled": 90003,
+        "reviewed": 16294,
+        "star": 4.6,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/1436092_2024_4.jpg"
+    },
+    {
+        "key": "MRDJ3453",
+        "title": "The Modern React Bootcamp - Hooks, Context, Router",
+        "instructor": "Colt Steele",
+        "regularPrice": "188.99",
+        "price": "19.99",
+        "enrolled": 20256,
+        "reviewed": 3944,
+        "star": 4.7,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/2320056_4fa0_6.jpg"
+    },
+    {
+        "key": "LHYFLD458",
+        "title": "Learn Nodejs by building 12 projects",
+        "instructor": "Eduonix-Tech",
+        "regularPrice": "49.99",
+        "price": "9.99",
+        "enrolled": 13754,
+        "reviewed": 1825,
+        "star": 4.1,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/481002_0155_3.jpg"
+    },
+    {
+        "key": "NJATE6493",
+        "title": "Node JS: Advanced Concepts",
+        "instructor": "Stephen Grider",
+        "regularPrice": "199.99",
+        "price": "15.99",
+        "enrolled": 31118,
+        "reviewed": 4985,
+        "star": 4.9,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/1587718_8fdf.jpg"
+    },
+    {
+        "key": "CDBRDKH54",
+        "title": "C++: From Beginner to Expert",
+        "instructor": "Arkadiusz Włodarczyk",
+        "regularPrice": "139.99",
+        "price": "21.99",
+        "enrolled": 39442,
+        "reviewed": 7895,
+        "star": 4.5,
+        "coverPhoto": "https://img-a.udemycdn.com/course/240x135/43718_e5ca_10.jpg"
     },
 
 ];
